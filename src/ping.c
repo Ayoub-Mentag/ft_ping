dbe8c037ba21211c7f380739bc6d51cef49ff4fb
Ayoub Mentag <amentag@e3r10p15.1337.ma>
2025-08-18 18:09:22 +0100

    remain -v

--- src/ping.c
+++ src/ping.c
@@ -1,20 +1,39 @@
 #include "ping.h"
+#include <netdb.h>
+
+int resolve_hostname(const char *hostname, struct in_addr *addr) {
+    struct addrinfo hints, *res;
+    int ret;
+
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET;      // Force IPv4
+    hints.ai_socktype = SOCK_RAW;   // Raw socket
+
+    ret = getaddrinfo(hostname, NULL, &hints, &res);
+    if (ret != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret));
+        return -1;
+    }
+
+    *addr = ((struct sockaddr_in *)res->ai_addr)->sin_addr;
+
+    freeaddrinfo(res);
+    return 0;
+}
 
 unsigned short in_cksum(unsigned short *addr, int count) {
-    unsigned long sum = 0;
+    unsigned int sum = 0;
     
-    while (count > 1) {
-        sum += *addr++;
+    while (count > 0) {
+        if (count != 1)
+            sum += *addr++;
+        else
+            sum += *(unsigned char *)addr++;
+        while (sum > UINT16_MAX)
+            sum = (sum >> 16) + (sum & 0xffff);
         count -= 2;
     }
-    
-    if (count > 0)
-        sum += *(unsigned char *)addr;
-    
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum += (sum >> 16);
-    
-    return (~sum);
+    return (unsigned short)(~sum);
 }
 
 void cleanup(int sig) {
@@ -26,7 +45,7 @@ void cleanup(int sig) {
         loss = 100.0 * (tx_count - rx_count) / tx_count;
     
     printf("%d packets transmitted, %d received, %.1f%% packet loss\n",
-           tx_count, rx_count, loss);
+            tx_count, rx_count, loss);
     
     if (sock >= 0) 
         close(sock);
@@ -97,14 +116,14 @@ void process_reply(char *recvbuf, int bytes, struct sockaddr_in *from,
         rx_count++;
         
         double rtt = (tv_end->tv_sec - tv_start->tv_sec) * 1000.0 +
-                   (tv_end->tv_usec - tv_start->tv_usec) / 1000.0;
+                (tv_end->tv_usec - tv_start->tv_usec) / 1000.0;
         
         printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.3f ms\n",
-               bytes - hlen,
-               inet_ntoa(from->sin_addr),
-               icmp_reply->icmp_seq,
-               ip_hdr->ip_ttl,
-               rtt);
+            bytes - hlen,
+            inet_ntoa(from->sin_addr),
+            icmp_reply->icmp_seq,
+            ip_hdr->ip_ttl,
+            rtt);
     }
 }
 
@@ -145,33 +164,91 @@ int ping_loop(int sock, struct sockaddr_in *dest) {
     return 0;
 }
 
+void handleFlag(char c) {
+if (c == '?') {
+        printf(
+            "Usage: ping [OPTION...] HOST ...\n"
+            "Send ICMP ECHO_REQUEST packets to network hosts.\n"
+            "\n"
+            "Options valid:\n"
+            "\n"
+            " -v,             verbose output\n"
+            " -?              give this help list\n"
+            "\n"
+        );
+        exit(0);
+    }
+    printf("Flag is %c\n", c);
+}
+
+void verifyFlag(char *argv, char *validFlags) {
+    if (strlen(argv) != 2) {
+        printf("Invalid flag %s\n", argv);
+        exit(1);
+    }
+    for (size_t i = 0; i < strlen(validFlags); i++) {
+        if (argv[1] == validFlags[i]) {
+            handleFlag(argv[1]);
+            return ;
+        }
+    }
+
+    printf("Invalid flag %s\n", argv);
+    exit(1);
+}
+
+void verifyAddr(char *argv) {
+    // Try to parse as raw IP first
+    if (inet_pton(AF_INET, argv, &dest_addr) <= 0) {
+        // If not a valid IP, try hostname resolution
+        struct in_addr resolved;
+        if (resolve_hostname(argv, &resolved) < 0) {
+            fprintf(stderr, "Could not resolve hostname: %s\n", argv);
+            exit(1);
+        }
+        dest_addr = resolved.s_addr;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s hostname\n", argv[0]);
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "Usage: %s [-v] hostname\n", argv[0]);
         return 1;
     }
-    
-    if (inet_pton(AF_INET, argv[1], &dest_addr) <= 0) {
-        fprintf(stderr, "Bad address: %s\n", argv[1]);
-        return 1;
+
+    // Handle single argument
+    if (argc == 2) {
+        if (argv[1][0] == '-') {
+            verifyFlag(argv[1], "?");
+            return 0;
+        }
+        verifyAddr(argv[1]);
     }
-    
-    signal(SIGINT, cleanup);
-    
+    else {
+        if (argv[1][0] == '-') {
+            verifyFlag(argv[1], "?v");
+            verifyAddr(argv[2]);
+        } else {
+            verifyFlag(argv[2], "?v");
+            verifyAddr(argv[1]);
+        }
+    }
+
     sock = init_socket();
     if (sock < 0) {
         return 1;
     }
-    
+    signal(SIGINT, cleanup);
+
     struct sockaddr_in dest;
     memset(&dest, 0, sizeof(dest));
     dest.sin_family = AF_INET;
     dest.sin_addr.s_addr = dest_addr;
-    
+
     printf("PING %s (%s): %ld data bytes\n", 
-           argv[1], 
-           inet_ntoa(*(struct in_addr *)&dest_addr), 
-           PKT_SIZE - sizeof(struct icmphdr));
+        argv[1], 
+        inet_ntoa(*(struct in_addr *)&dest_addr), 
+        PKT_SIZE - sizeof(struct icmphdr));
     
     return ping_loop(sock, &dest);
 }
